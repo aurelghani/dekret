457ac420580b508d3235323cf2bf1271e45f77ef
Aurel Arta <aurelghani123@gmail.com>
2026-04-04 21:15:45 +0700

    statistik persen persenan

--- game.h
+++ game.h
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
 
+struct Statistik {
+    int ekonomi = 50;
+    int masyarakat = 50;
+    int militer = 50;
+    int lingkungan = 50;
+};
+
+Statistik statistik;
+
+void tampilkanStatistik() {
+    cout << "-----------------------------" << endl;
+    cout << "Ekonomi     : " << statistik.ekonomi << "%" << endl;
+    cout << "Masyarakat  : " << statistik.masyarakat << "%" << endl;
+    cout << "Militer     : " << statistik.militer << "%" << endl;
+    cout << "Lingkungan  : " << statistik.lingkungan << "%" << endl;
+    cout << "-----------------------------" << endl;
+}
+
+int main() {
+    tampilkanStatistik();
+    return 0;
+}
